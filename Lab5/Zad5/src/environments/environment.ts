9cd31b91e3ffce8ae699ef8415008cafb8b434dd
Kasl0 <slonieckacper@gmail.com>
2022-12-20 01:48:57 +0100

    Lab5 Zad5 init

---
+++ Lab5/Zad5/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyD82WLklJ403sgVX1LzJBletDxMpCMArm4",
+    authDomain: "studenci-266da.firebaseapp.com",
+    projectId: "studenci-266da",
+    storageBucket: "studenci-266da.appspot.com",
+    messagingSenderId: "43744748167",
+    appId: "1:43744748167:web:941ea0bb48bb1f40764e22"
+  }
+
+};
